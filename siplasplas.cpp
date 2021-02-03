124be31456f50938952f530272a44a60d2a3952a
atakankillioglu <atakankillioglu@users.noreply.github.com>
2021-02-03 19:03:43 +0300

    cleaning
    deleted unsused code

--- siplasplas.cpp
+++ siplasplas.cpp
@@ -1,15 +1,12 @@
 #include<iostream>
 
-int rt_zero (int a){
-    
-    return 0;
-}
+int rt_zero (){ return 0; }
 
 int main(){
 
     std::cout<<"Hi Woe!";
     std::cout<<"  im awesum";
-    
+
     std::cout<< rt_zero;
 
     return 0;
