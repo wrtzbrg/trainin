41669feecf32b069b24ea4e09a2d71007f4d7a97
atakankillioglu <atakankillioglu@users.noreply.github.com>
2021-02-03 19:00:58 +0300

    rt zero func
    rt zero function created

--- siplasplas.cpp
+++ siplasplas.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+
+int rt_zero (int a){
+    
+    return 0;
+}
+
 int main(){
 
     std::cout<<"Hi Woe!";
     std::cout<<"  im awesum";
     
+    std::cout<< rt_zero;
+
     return 0;
 }
