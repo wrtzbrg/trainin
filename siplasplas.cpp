aa7a73f77994ced8d321814b6320453d610a1693
atakankillioglu <atakankillioglu@users.noreply.github.com>
2021-02-03 19:32:44 +0300

    added func
    a func returns max of two int

--- siplasplas.cpp
+++ siplasplas.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+int max_of_two(int,int);
 bool rt_true(){return true; }
 
 int rt_zero (){ return 0; }
@@ -12,4 +12,11 @@ int main(){
     std::cout<< rt_zero;
 
     return 0;
+}
+
+int max_of_two(int a, int b){
+    if a>b :
+        return a;
+    else:
+        return b;
 }
