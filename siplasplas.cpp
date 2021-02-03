8b55cb40474c57509cb241ed2fca7b1805dda870
wrtzbrg <atakankillioglu@gmail.com>
2021-02-03 19:28:32 +0300

    added features are;
    none

--- siplasplas.cpp
+++ siplasplas.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 
-bool rt_true(){return true; }
+bool rt_true(){ return true; }
 
-int rt_zero (){ return 0; }
+int rt_zero(){ return 0; }
 
 int main(){
 
-    std::cout<<"Hi Woe!";
-    std::cout<<"  im awesum";
+    std::cout<<"Hi Woe!\n";
+    std::cout<<"im awesum\n";
 
-    std::cout<< rt_zero;
+    int i = rt_zero();
+    std::cout<< i << std::endl;
+
+    bool bull = rt_true;
+    std::cout<< bull <<std::endl;
 
     return 0;
 }
